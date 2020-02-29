35c51431ba24d0261c9d3628b83fdb5ad66acd66
MarinPuizina <marinpuizina@hotmail.com>
2020-02-29 11:58:26 +0100

    Add model package and CoronavirusData class

---
+++ src/main/java/com/marin/coronavirusapp/model/CoronavirusData.java
@@ -0,0 +1,11 @@
+package com.marin.coronavirusapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CoronavirusData {
+}
