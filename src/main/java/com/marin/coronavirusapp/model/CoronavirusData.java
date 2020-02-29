c9edca9ff77ea205503658b867755d1d072fcc6c
MarinPuizina <marinpuizina@hotmail.com>
2020-02-29 13:06:58 +0100

    Refactor CoronavirusData class

--- src/main/java/com/marin/coronavirusapp/model/CoronavirusData.java
+++ src/main/java/com/marin/coronavirusapp/model/CoronavirusData.java
@@ -4,13 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CoronavirusData {
 
+    public static List<CoronavirusData> virusData = new ArrayList<>();
+
     private String State;
     private String Country;
-    private int newCases;
+    private int NumberOfCases;
 
 }
