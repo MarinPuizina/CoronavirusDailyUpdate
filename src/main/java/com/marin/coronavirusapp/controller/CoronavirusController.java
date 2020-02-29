0295c9327f8fe1f781b5698234e331d73cfd23bf
MarinPuizina <marinpuizina@hotmail.com>
2020-02-29 12:53:17 +0100

    Add default Thymeleaf layout for html

--- src/main/java/com/marin/coronavirusapp/controller/CoronavirusController.java
+++ src/main/java/com/marin/coronavirusapp/controller/CoronavirusController.java
@@ -1,13 +1,16 @@
 package com.marin.coronavirusapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CoronavirusController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+
+        model.addAttribute("test", "TEST");
 
         return "home";
     }
